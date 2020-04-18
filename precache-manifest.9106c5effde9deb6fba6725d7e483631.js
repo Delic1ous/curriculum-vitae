3c58a60e2476d7f311588833f82a9b96b91eea95
Delic1ous <s.delic1ous.d@gmail.com>
2020-04-18 23:35:38 +0300

    Updates

---
+++ precache-manifest.9106c5effde9deb6fba6725d7e483631.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8347e93b5deed3d9e839e6e183ccdd4f",
+    "url": "/curriculum-vitae/index.html"
+  },
+  {
+    "revision": "31dfebd6ee417e55ecca",
+    "url": "/curriculum-vitae/static/js/2.2baf546a.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/curriculum-vitae/static/js/2.2baf546a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed0d253d295b41fb03f",
+    "url": "/curriculum-vitae/static/js/main.087c9c0c.chunk.js"
+  },
+  {
+    "revision": "0cd5c4d799dcc97324bf",
+    "url": "/curriculum-vitae/static/js/runtime-main.59475df7.js"
+  },
+  {
+    "revision": "fd7f00df89041fe001bc91b709c06157",
+    "url": "/curriculum-vitae/static/media/Avatar.fd7f00df.jpg"
+  },
+  {
+    "revision": "a852e411c8a7b9670db968fa18341b81",
+    "url": "/curriculum-vitae/static/media/Avatar2.a852e411.jpg"
+  },
+  {
+    "revision": "a5dd11c60ff19ed6e414c72bfb7c3911",
+    "url": "/curriculum-vitae/static/media/CV -Dmitry Sauliak.a5dd11c6.pdf"
+  }
+]);
